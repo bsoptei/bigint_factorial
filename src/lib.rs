02f885dc1742b71e81afa08ecd42e3e8d7247bb4
bsoptei <bsoptei@gmail.com>
2018-04-14 14:11:58 +0200

    First version

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+mod functions;
+
+#[cfg(test)]
+mod tests {
+
+    use functions::factorial::*;
+
+    #[test]
+    fn factorial_2000() {
+        let result = bigint_factorial(2000);
+        let first_32_digits = &result[..32];
+        assert_eq!("33162750924506332411753933805763", first_32_digits);
+        assert_eq!(result.len(), 5736);
+    }
+}
