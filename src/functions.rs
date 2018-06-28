13d934ae64ec5d19fece8b21f6aa0930860b4368
bsoptei <bsoptei@gmail.com>
2018-06-28 20:53:10 +0200

    use ..= for inclusive

--- src/functions.rs
+++ src/functions.rs
@@ -3,19 +3,14 @@ pub mod factorial {
     use self::num_bigint::{ToBigInt, BigInt};
 
     pub fn bigint_factorial(i: u32) -> String {
-        bigint_range(1, i, true).into_iter()
-        .fold( bigint(1), |product, val| product * val)
+        (1..=i).into_iter()
+        .map(|x| bigint(x))
+        .fold(bigint(1), |product, val| product * val)
         .to_str_radix(10)
     }
 
     fn bigint(i: u32) -> BigInt {
         i.to_bigint().unwrap()
-    }
-
-    fn bigint_range(from: u32, to: u32, inclusive: bool) -> Vec<BigInt> {
-        let corr = if inclusive { 1 } else { 0 };
-        (from..to + corr).into_iter()
-        .map(|x| bigint(x)).collect()
     }
 
 }
