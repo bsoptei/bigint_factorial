2627acdb353d3cbef426fd205d1dbb00582a32af
bsoptei <bsoptei@gmail.com>
2018-07-24 22:26:12 +0200

    replace if with match in main

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,25 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    if args.len() >= 2 {
-        let main_arg = &args[1];
+    match args.get(1) {
+        Some(main_arg) => {
+            match main_arg.parse::<u32>() {
+                Ok(n) => {
+                    println!("I'm calculating the factorial of {}", n);
 
-        match main_arg.parse::<u32>() {
-            Ok(n) => {
-                println!("I'm calculating the factorial of {}", n);
+                    let result = bigint_factorial(n).to_str_radix(10);
+                    let shown_length = if &result.len() < &32 { result.len() } else { 32 };
 
-                let result = bigint_factorial(n).to_str_radix(10);
-                let shown_length = if &result.len() < &32 { result.len() } else { 32 };
-
-                println!(
-                    "Done. Result:\n{}.{}E{}",
-                    &result[..1],
-                    &result[1..shown_length],
-                    result.len() - 1
-                )
-            },
-            Err(_e) => println!("Could not parse {}", main_arg)
-        }
-    } else {
-        println!("No argument, no result")
+                    println!(
+                        "Done. Result:\n{}.{}E{}",
+                        &result[..1],
+                        &result[1..shown_length],
+                        result.len() - 1
+                    )
+                },
+                Err(_e) => println!("Could not parse {}", main_arg)
+            }
+        },
+        None => println!("No argument, no result")
     }
-
 }
